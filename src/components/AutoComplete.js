c160dcc3047335f6b337aae0eef3abfd774ea7c6
Curt Hoyt <choyt@users.noreply.github.com>
2017-07-21 01:54:25 -0700

    HOC hackery

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import injectSheet from 'react-jss';
-// import { withState } from 'recompose';
+// import { compose, withHandlers, withState } from 'recompose';
 
 const styles = {
   AutoComplete: {
@@ -12,23 +12,44 @@ const styles = {
     margin: '0',
     padding: '0',
   },
+  ListItem: {
+    cursor: 'pointer',
+  },
 };
 
-// const withCondition = conditionalRenderingFn => Component => props =>
-//  conditionalRenderingFn(props) ? null : <Component {...props} />;
+// const enhance = compose(
+//   withState('value', 'inputValue', ''),
+//   withHandlers({
+//     onChange: props => event => {
+//       props.updateValue(event.target.value);
+//     }
+//   })
+// )
 
-const AutoComplete = ({ options, classes }) =>
+const AutoComplete = ({
+  classes,
+  handleChange,
+  handleKeyDown,
+  inputValue,
+  options,
+}) =>
   <div className={classes.AutoComplete}>
     <div className={classes.ComboBox}>
       <input
-        type="text"
         aria-autocomplete="list"
+        aria-label="input"
+        onChange={handleChange} //
+        onKeyDown={handleKeyDown} //
+        type="text"
+        value={inputValue} //
       />
     </div>
     <ul className={classes.ListBox} role="listbox">
-      {options.map((option, idx) => {
-        return <li key={idx}>{option}</li>;
-      })}
+      {options.map((option, idx) =>
+        <li className={classes.ListItem} key={idx}>
+          {option}
+        </li>
+      )}
     </ul>
   </div>;
 
@@ -53,4 +74,17 @@ export default injectSheet(styles)(AutoComplete);
     <li role="option">Zebra</li>
     <li role="option" id="selected_option">Zoom</li>
 </ul>
+
+const addCounting = compose(
+  withState('counter', 'setCounter', 0),
+  withHandlers({
+    increment: ({ setCounter }) => () => setCounter(n => n + 1),
+    decrement: ({ setCounter }) => () =>  setCounter(n => n - 1),
+    reset: ({ setCounter }) => () => setCounter(0)
+  })
+)
+
+
+const withCondition = conditionalRenderingFn => Component => props =>
+  conditionalRenderingFn(props) ? null : <Component {...props} />;
 */
